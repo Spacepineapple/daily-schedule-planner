1aaeb7e7ea4aac9d0255583a67cec3b3a302dee2
Spacepineapple <neal.t.reeves@gmail.com>
2023-01-24 11:55:20 +0000

    Replace call to event.target in saveSchedule with event.currentTarget

--- script.js
+++ script.js
@@ -3,50 +3,75 @@ let currentDayEl = $("#currentDay");
 let scheduleContainer = $(".container");
 let currentHour = getCurrentHour();
 
+//Get today's date
 function displayDate() {
     currentDayEl.text(today);
 }
 
+//Get the hour of the current time 
 function getCurrentHour() {
     return moment().format("H")
 }
 
 function initialiseSchedule() {
+    //Iterate through times from 9AM to 5PM
     for (let i=9; i<18; i++) {
         let time = moment(`${i}`, "h").format("hA");
-        let timeBlock = $("<div>");
-        timeBlock.attr("class", "time-block");
+        let timeBlock = createTimeBlock(i);
         let row = $("<div>");
         row.attr("class", "row");
         scheduleContainer.append(timeBlock);
         timeBlock.append(row);
-        let hour = $("<div>");
-        hour.attr("class", "col-1 hour");
+        let hour = createHour();
         hour.text(`${time}`);
         row.append(hour);
-        let text = $("<textarea>");
-        text.attr("class", "col-10");
+        let text = createTextArea();
         text.val(getSchedule(time));
-        if (i<currentHour) {
-            timeBlock.addClass("past");
-        } else if (i>currentHour) {
-            timeBlock.addClass("future");
-        } else {
-            timeBlock.addClass("present");
-        }
         row.append(text);
-        let save = $("<div>");
-        save.attr("class", "col-1 saveBtn d-flex justify-content-center align-items-center");
-        let icon = $("<i>");
-        icon.attr("class", "fa-regular fa-floppy-disk");
-        save.append(icon);
+        let save = createSaveButton();
         row.append(save);
     }
     $(".saveBtn").on("click", saveSchedule);
 }
 
+function createTimeBlock(i) {
+    let timeBlock = $("<div>");
+    timeBlock.attr("class", "time-block");
+    if (i<currentHour) {
+        timeBlock.addClass("past");
+    } else if (i>currentHour) {
+        timeBlock.addClass("future");
+    } else {
+        timeBlock.addClass("present");
+    }
+    return timeBlock;
+}
+
+function createHour() {
+    let hour = $("<div>");
+    hour.attr("class", "col-1 hour");
+    return hour;    
+}
+
+function createTextArea() {
+    let text = $("<textarea>");
+    text.attr("class", "col-10");
+    return text;    
+}
+
+function createSaveButton() {
+    let save = $("<div>");
+    save.attr("class", "col-1 saveBtn d-flex justify-content-center align-items-center");
+    let icon = $("<i>");
+    icon.attr("class", "fa-regular fa-floppy-disk");
+    save.append(icon);
+    return save;
+}
+
+//Get any saved schedule descriptions from local storage
 function getSchedule(key) {
     let description = localStorage.getItem(key);
+    //If there is no scheduled event for this time, return an empty string
     if (description === null) {
         return "";
     } else {
@@ -54,13 +79,15 @@ function getSchedule(key) {
     }
 }
 
+//Save schedule description text to local storage
 function saveSchedule(event) {
-    let parentEl = event.target.parentNode;
+    let parentEl = event.currentTarget.parentNode;
     let timeSlot = $(parentEl).children(".hour").text();
     let description = $(parentEl).children("textarea").val();
     localStorage.setItem(timeSlot, description);
 }
 
+//Update past, present and future classes assigned to blocks of time
 function refreshStatus() {
     $('.time-block').each(function(i) {
         currentHour = getCurrentHour();
